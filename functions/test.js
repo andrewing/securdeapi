6d576d7de8c263095c91914e2fbec4fc9187d710
andrewing <john_andrew_ing@dlsu.edu.ph>
2020-04-16 16:12:59 +0800

    feat: fix db

--- functions/test.js
+++ functions/test.js
@@ -5,8 +5,7 @@ export const handler = (route,event, context, callback) => {
   callback(
     null,
     CODE(200, `This test is with name ${faker.name.findName()}!`, {
-      randomName: faker.name.findName(),
-      env: process.env
+      randomName: faker.name.findName()
     }),
   );
 };
